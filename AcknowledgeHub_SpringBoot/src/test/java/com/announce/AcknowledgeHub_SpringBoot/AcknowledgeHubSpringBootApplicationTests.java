35274cb90f21dc5f8572ee5ff898a5b8b9ffc066
Toe Yadana Kyaw <50312009+toeyadanakyaw@users.noreply.github.com>
2024-08-07 15:39:21 +0630

    project initialization

---
+++ AcknowledgeHub_SpringBoot/src/test/java/com/announce/AcknowledgeHub_SpringBoot/AcknowledgeHubSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.announce.AcknowledgeHub_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcknowledgeHubSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
